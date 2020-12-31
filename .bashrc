2d676ff9a3706db3af1654d22f45ca217619526e
Thang Pham <phamducthang1234@gmail.com>
2020-12-31 12:45:14 +0900

    backup configs 20/12/31

--- .bashrc
+++ .bashrc
@@ -8,7 +8,7 @@
 # run tmux in normal shell (fish included),
 # run fish in emacs shell.
 if [[ $DISPLAY ]] && ! [[ $INSIDE_EMACS ]]; then
-    [[ -z "$TMUX" ]] && exec tmux
+    [[ -z "$TMUX" ]] && (tmux attach >/dev/null 2>&1 || tmux)
 else
     exec fish
 fi
--- backup-config-files.py
+++ backup-config-files.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 import os
 from pathlib import Path
 
@@ -5,26 +7,47 @@ home = str(Path.home())
 
 home_white_list = [
     ".bash_profile", ".bashrc", ".spacemacs", ".tmux.conf", ".vimrc",
-    ".xbindkeysrc", ".xkbcomp", ".tmux", ".ssh", ".pki", ".gnupu"
+    ".xbindkeysrc", ".xkbcomp", ".tmux", ".ssh", ".pki", ".gnupu", ".gitconfig"
 ]
 
-dot_config_ignore_list = [
-    "chromium",
-    "discord",
-    "discordcanary",
-    "gtk-3.0",
-    "keybase",
-    "Keybase",
+dot_config_white_list = [
+    "autorandr",
+    "autostart",
+    "dconf",
+    "fish",
+    "flake8",
+    "flameshot",
+    "fontconfig",
+    "i3",
+    "i3status",
+    "kak",
+    "kak-lsp",
+    "kitty",
+    "light",
     "mimeapps.list",
-    "nautilus",
-    "nemo",
-    "pavucontrol.ini",
-    "pulse",
-    "spotify",
-    "yarn",
+    "picom.conf",
+    "yapf",
 ]
 
 cwd = os.path.dirname(__file__)
+
+# add git modules first
+if os.path.exists('{}/.config/kak/plugins/plug.kak'.format(cwd)):
+  os.system(
+      'git submodule add https://github.com/robertmeta/plug.kak.git {}/.config/kak/plugins/plug.kak'
+      .format(cwd))
+if os.path.exists('{}/.config/kitty/kitty-themes'.format(cwd)):
+  os.system(
+      'git submodule add https://github.com/dexpota/kitty-themes {}/.config/kitty/kitty-themes'
+      .format(cwd))
+if os.path.exists('{}/.tmux/plugins/tpm'.format(cwd)):
+  os.system(
+      'git submodule add https://github.com/tmux-plugins/tpm {}/.tmux/plugins/tmp'
+      .format(cwd))
+if os.path.exists('{}/.tmux/plugins/tmux-themepack'.format(cwd)):
+  os.system(
+      'git submodule add https://github.com/jimeh/tmux-themepack {}/.tmux/plugins/tmux-themepack'
+      .format(cwd))
 
 for f in os.listdir(home):
   if f in home_white_list:
@@ -32,5 +55,5 @@ for f in os.listdir(home):
 
 os.system('mkdir -p .config')
 for f in os.listdir('{}/.config'.format(home)):
-  if f not in dot_config_ignore_list:
+  if f in dot_config_white_list:
     os.system('cp -r ~/.config/{} {}/.config'.format(f, cwd))
