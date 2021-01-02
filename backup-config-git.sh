a8855278399efbb9cf5369bf697ba6ea1a140487
Thang Pham <phamducthang1234@gmail.com>
2021-01-02 10:37:48 +0900

    backup configs 01/02/21 10:37:48

--- backup-config-git.sh
+++ backup-config-git.sh
@@ -4,5 +4,5 @@ echo $HOME
 cd $HOME/Backup/my-config-files/
 ./backup-config-files.py
 git add .
-git commit --allow-empty -m "backup configs $(date +"%D %T")"
+git commit -m "backup configs $(date +"%D %T")"
 git push
