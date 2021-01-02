0bc355e3f222fd50cc3fd77d3bec0d04723e5b9b
Thang Pham <phamducthang1234@gmail.com>
2021-01-02 10:25:31 +0900

    backup configs 01/02/21 10:25:31

--- backup-config-files.py
+++ backup-config-files.py
@@ -34,19 +34,19 @@ cwd = os.path.dirname(__file__)
 # add git modules first
 if os.path.exists('{}/.config/kak/plugins/plug.kak'.format(cwd)):
   os.system(
-      'git submodule add https://github.com/robertmeta/plug.kak.git {}/.config/kak/plugins/plug.kak'
+      'git submodule add https://github.com/robertmeta/plug.kak.git {}/.config/kak/plugins/plug.kak 2>/dev/null'
       .format(cwd))
 if os.path.exists('{}/.config/kitty/kitty-themes'.format(cwd)):
   os.system(
-      'git submodule add https://github.com/dexpota/kitty-themes {}/.config/kitty/kitty-themes'
+      'git submodule add https://github.com/dexpota/kitty-themes {}/.config/kitty/kitty-themes 2>/dev/null'
       .format(cwd))
 if os.path.exists('{}/.tmux/plugins/tpm'.format(cwd)):
   os.system(
-      'git submodule add https://github.com/tmux-plugins/tpm {}/.tmux/plugins/tmp'
+      'git submodule add https://github.com/tmux-plugins/tpm {}/.tmux/plugins/tmp 2>/dev/null'
       .format(cwd))
 if os.path.exists('{}/.tmux/plugins/tmux-themepack'.format(cwd)):
   os.system(
-      'git submodule add https://github.com/jimeh/tmux-themepack {}/.tmux/plugins/tmux-themepack'
+      'git submodule add https://github.com/jimeh/tmux-themepack {}/.tmux/plugins/tmux-themepack 2>/dev/null'
       .format(cwd))
 
 for f in os.listdir(home):
--- backup-config-git.sh
+++ backup-config-git.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -e
-id >&2
-cd /home/aome510/Backup/my-config-files/
+HOME=/home/aome510
+echo $HOME
+cd $HOME/Backup/my-config-files/
 ./backup-config-files.py
 git add .
 git commit --allow-empty -m "backup configs $(date +"%D %T")"
-# git push
+git push
