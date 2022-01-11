113555a686c8b1ec76fe1249e6ecde33f3f82e23
Thang Pham <phamducthang1234@gmail.com>
2022-01-10 21:25:40 -0500

    remove redundant files in .doom.d

--- .doom.d/vue.el
+++
@@ -1,7 +0,0 @@
-;;; vue.el -*- lexical-binding: t; -*-
-
-(define-derived-mode vue-mode web-mode "vue-mode"
-  "A major derived from web-mode for editing .vue files")
-
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
-(add-hook! 'vue-mode-hook #'lsp!)
